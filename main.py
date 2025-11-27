ae17ea3dd2fbe7a5a4f01b6718c45460becba179
quenchhe <quentin.chhean@gmail.com>
2025-11-27 17:43:08 +0100

    setting up pre-commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from xiplusplus!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ test_format.py
@@ -0,0 +1,13 @@
+def badly_formatted_function(x, y, z):
+    if x > 0:
+        result = x + y * z
+        return result
+    else:
+        return None
+
+
+def another_function(a, b):
+    data = [1, 2, 3, 4, 5]
+    for i in data:
+        print(f"Value: {i}")
+    return a + b
